19962704c0027fac3f4a9237e70b800134f7e52c
easylife9748 <36080843+easylife9748@users.noreply.github.com>
2018-03-15 15:55:22 +0100

    Create calendrier.py

---
+++ calendrier.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        #print(start, event['summary'])
+        t=str(event['start'].get('dateTime'))
+        
+        print( event['start'].get('dateTime'))
+        #for i in range(4):
+        try:
+            y=t[0]+t[1]+t[2]+t[3]
+            print( y),
+            m=t[5]+t[6]
+            print( m),
+            d=t[8]+t[9]
+            print(d),
+        except IndexError:
+            pass
+        try:
+            h=t[11]+t[12]
+            print( h),
+            min=t[14]+t[15]
+            print( min),
+
+        except IndexError:
+            pass
+
+        
+##        mon_fichier = open("/home/pi/speak.txt", "w")
+##        mon_fichier.write(  str(event['start']))
+##        mon_fichier = open("/home/pi/speak.txt", "r")
+##        os.system("espeak -ven+m3 \"upcomping event at \"")
+##        os.system("espeak -v en -f speak.txt " )
+##        
+##        
+##        mon_fichier = open("/home/pi/speak.txt", "w")
+##        mon_fichier.write(  str(event['summary']))
+##        mon_fichier = open("/home/pi/speak.txt", "r")
+##        os.system("espeak -ven+m3 \"the description is\"")
+##        os.system("espeak -v en -f speak.txt " )
+
+
+if __name__ == '__main__':
+    main()
+
